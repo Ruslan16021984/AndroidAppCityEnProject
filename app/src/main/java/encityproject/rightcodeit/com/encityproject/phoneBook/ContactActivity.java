1f779974e7d1f44e5b479a7b1782fad1b2a80f1d
Yelinskyi <Yelinskyizp@gmail.com>
2019-11-29 15:59:06 +0200

    Add alerdialog

--- app/src/main/java/encityproject/rightcodeit/com/encityproject/phoneBook/ContactActivity.java
+++ app/src/main/java/encityproject/rightcodeit/com/encityproject/phoneBook/ContactActivity.java
@@ -1,5 +1,9 @@
 package encityproject.rightcodeit.com.encityproject.phoneBook;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,8 +11,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import encityproject.rightcodeit.com.encityproject.R;
 
 public class ContactActivity extends AppCompatActivity {
@@ -17,11 +26,17 @@ public class ContactActivity extends AppCompatActivity {
     ContactAdapter adapter;
 
     private int index;
-
     private String nameContact;
     private String phoneNumber;
+    private String phone1;
+    private String phone2;
+    private String returnPhone;
+    final Context context = this;
+    private Button btnPhone1;
+    private Button btnPhone2;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
@@ -30,17 +45,64 @@ public class ContactActivity extends AppCompatActivity {
     }
 
     private void initData() {
-
         adapter = new ContactAdapter(this, arrayOfContacts.getArrayOfContacts());
         ListView listView = findViewById(R.id.lv_contact);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: " + arrayOfContacts.getArrayOfContacts().get(position).getPhoneNumber()));
-                if (intent != null) {
-                    startActivity(intent);
+//////////////////////////////////////
+                final String[] split;
+                String toSplit = arrayOfContacts.getArrayOfContacts().get(position).getPhoneNumber();
+                split = toSplit.split(",");
+
+/*                String toToast;
+                if (split.length < 2) {
+                    toToast = split[0];
+                } else {
+                    toToast = split[0] + split[1];
                 }
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        (toToast),
+                        Toast.LENGTH_SHORT);
+                toast.show();*/
+
+                if (split.length < 2) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse
+                            ("tel: " + arrayOfContacts.getArrayOfContacts().get(position).getPhoneNumber()));
+                    if (intent != null) {
+                        startActivity(intent);
+                    }
+                } else {
+                    final Dialog dialog = new Dialog(context);
+                    dialog.setContentView(R.layout.custom_dialog);
+                    btnPhone1 = (Button) dialog.findViewById(R.id.phone1);
+                    btnPhone2 = (Button) dialog.findViewById(R.id.phone2);
+                    btnPhone1.setText(split[0]);
+                    btnPhone2.setText(split[1]);
+                    dialog.show();
+
+                    btnPhone1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: " + split[0]));
+                            if (intent != null) {
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                    btnPhone2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: " + split[1]));
+                            if (intent != null) {
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                }
+//////////////////////////////////////
+
             }
         });
     }
@@ -51,15 +113,13 @@ public class ContactActivity extends AppCompatActivity {
         if (data == null) {
             return;
         }
-
         ArrayList<String> oneContact;
         oneContact = data.getStringArrayListExtra("name");
         nameContact = oneContact.get(0);
         phoneNumber = oneContact.get(1);
-
         arrayOfContacts.getArrayOfContacts().set(index, new Contact(nameContact, phoneNumber));
-
         adapter.notifyDataSetChanged();
     }
+
 }
 
--- app/src/main/java/encityproject/rightcodeit/com/encityproject/phoneBook/ContactAdapter.java
+++ app/src/main/java/encityproject/rightcodeit/com/encityproject/phoneBook/ContactAdapter.java
@@ -5,7 +5,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
 import java.util.ArrayList;
+
 import encityproject.rightcodeit.com.encityproject.R;
 
 public class ContactAdapter extends ArrayAdapter<Contact> {
@@ -31,7 +33,14 @@ public class ContactAdapter extends ArrayAdapter<Contact> {
         final TextView tvPhoneNumber = (TextView) convertView.findViewById(R.id.tv_phone_number);
 
         tvNameContact.setText(contact.getNameContact());
-        tvPhoneNumber.setText(contact.getPhoneNumber());
+        final String[] split;
+        String toSplit = contact.getPhoneNumber();
+        split = toSplit.split(",");
+        if (split.length > 1) {
+            tvPhoneNumber.setText(split[0] + split[1]);
+        } else {
+            tvPhoneNumber.setText(contact.getPhoneNumber());
+        }
 
         return convertView;
     }
